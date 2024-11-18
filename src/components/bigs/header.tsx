ec8b6604f10e6b66f5f74c9df27b32929d248054
WinLuyeye <winnerluyeye1@gmail.com>
2024-11-18 12:07:47 +0100

    Add Header NavBar Responsive

---
+++ src/components/bigs/header.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { FaSignInAlt } from "react-icons/fa";
+import logo from "@/public/logo-inline-green.png";
+
+const Header: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className="flex items-center justify-between  text-customBlack px-6 py-4">
+      {/* Logo */}
+      <div className="flex items-center">
+        <Image src={logo} width={150} height={50} alt="logo" priority />
+      </div>
+
+      {/* Navigation */}
+      <nav>
+        {/* Hamburger Button */}
+        <button
+          className="block md:hidden focus:outline-none"
+          onClick={toggleMenu}
+          aria-label="Toggle navigation"
+        >
+          <div
+            className={`w-6 h-1 bg-white mb-1 transition-transform ${
+              isMenuOpen ? "rotate-45 translate-y-2" : ""
+            }`}
+          ></div>
+          <div
+            className={`w-6 h-1 bg-white mb-1 transition-opacity ${
+              isMenuOpen ? "opacity-0" : ""
+            }`}
+          ></div>
+          <div
+            className={`w-6 h-1 bg-white transition-transform ${
+              isMenuOpen ? "-rotate-45 -translate-y-2" : ""
+            }`}
+          ></div>
+        </button>
+
+        {/* Links and Connexion Button */}
+        <div
+          className={`absolute md:static top-[8%] left-0 w-full md:w-auto md:flex md:items-center bg-gray-800 md:bg-transparent transition-transform mb-4 ${
+            isMenuOpen ? "block" : "hidden"
+          }`}
+        >
+          <ul className="flex flex-col md:flex-row md:ml-4 w-full md:w-auto">
+            <li className="md:ml-4">
+              <a
+                href="#link1"
+                className="block py-2 px-4 hover:bg-gray-700 md:hover:bg-transparent text-customBlack "
+              >
+                Accueil
+              </a>
+            </li>
+            <li className="md:ml-4">
+              <a
+                href="#link2"
+                className="block py-2 px-4 hover:bg-gray-700 md:hover:bg-transparent text-customBlack "
+              >
+                À propos
+              </a>
+            </li>
+            <li className="md:ml-4">
+              <a
+                href="#link3"
+                className="block py-2 px-4 hover:bg-gray-700 md:hover:bg-transparent text-customBlack "
+              >
+                Services
+              </a>
+            </li>
+            <li className="md:ml-4">
+              <a
+                href="#link4"
+                className="block py-2 px-4 hover:bg-gray-700 md:hover:bg-transparent text-customBlack "
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+
+          {/* Connexion Button (positionné en bas sur mobile) */}
+          <button className="md:hidden flex items-center text-gray-200 py-2 px-4 border border-gray-500 hover:bg-gray-100 hover:text-primary transition duration-200 ease-in-out mb-4 ml-4">
+            <FaSignInAlt className="mr-2 text-gray-400 hover:text-primary transition duration-200 ease-in-out" />
+            Connexion
+          </button>
+        </div>
+      </nav>
+
+      {/* Ce bouton sera visible seulement sur les écrans medium et plus grands */}
+      <button className="hidden md:flex items-center text-gray-200 py-2 px-4 border border-gray-500 hover:bg-gray-100 hover:text-primary transition duration-200 ease-in-out">
+        <FaSignInAlt className="mr-2 text-gray-400 hover:text-primary transition duration-200 ease-in-out" />
+        Connexion
+      </button>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,19 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        customGray: '#eeeff2',
+        customBlack: '#1d1e21',
+        primary : '#048996',
+        primaryHover : '#037883'
       },
     },
   },
